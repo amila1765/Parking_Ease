f061f7b0a7883a036a0fa8a34f45462bbd6e2ed6
amila1765 <79371904+amila1765@users.noreply.github.com>
2024-12-27 03:04:11 +0530

    Add navigation bar model

---
+++ Pages/navbar.php
@@ -0,0 +1,12 @@
+<header>
+    <nav>
+        <div class="logo">ParkingEase</div>
+        <ul class="nav-links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#" onclick="openModal()">Reservation</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="reviews.php">Reviews</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+</header>
