5ac6b5da774a1dae8fdb061d2b1ac45033b7c01e
amila1765 <79371904+amila1765@users.noreply.github.com>
2024-12-30 14:07:06 +0530

    Updated navigation bar

--- Includes/navbar.php
+++ Includes/navbar.php
@@ -2,17 +2,17 @@
     <nav>
         <div class="logo">ParkingEase</div>
         <ul class="nav-links">
-        <?php
+            <?php
             // Detect the base path dynamically
             $basePath = (basename($_SERVER['SCRIPT_FILENAME']) === 'index.php') ? './' : '../';
 
             // Generate dynamic links
             ?>
-            <li><a href="index.php">Home</a></li>
+             <li><a href="<?= $basePath ?>index.php">Home</a></li>
             <li><a href="#" onclick="openModal()">Reservation</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="reviews.php">Reviews</a></li>
-            <li><a href="contact.php">Contact</a></li>
+            <li><a href="<?= $basePath ?>pages/about.php">About</a></li>
+            <li><a href="<?= $basePath ?>pages/reviews.php">Reviews</a></li>
+            <li><a href="<?= $basePath ?>pages/contact.php">Contact</a></li>
         </ul>
     </nav>
 </header>
